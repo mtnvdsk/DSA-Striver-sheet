9cfaa3d6bb3d8d593bc563a481cb8a7dc4cef543
mtnvdsk <mtnvdsk@gmail.com>
2024-02-06 16:03:12 +0530

    Arrays

---
+++ Arrays/Medium/superior element.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+
+    public static List< Integer > superiorElements(int []a) {
+        ArrayList<Integer> ans=new ArrayList<>();
+        int n=a.length;
+        int max=a[n-1];
+        ans.add(a[n-1]);
+        for(int i=n-2;i>=0;i--){
+            if(a[i]>max){
+                ans.add(a[i]);
+                max=a[i];
+            }
+        }
+        return ans;
+    }
+}
