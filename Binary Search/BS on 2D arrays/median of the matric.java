7b3284bb69198a6339ddb15668fff8648111f2cd
mtnvdsk <mtnvdsk@gmail.com>
2024-02-14 20:45:52 +0530

    Binary search on 2D

--- Binary Search/BS on 2D arrays/median of the matric.java
+++ Binary Search/BS on 2D arrays/median of the matric.java
@@ -80,3 +80,46 @@ public final class Solution
     }
 
 }
+
+//solution2
+public final class Solution {
+    public static int countSmallerThanMid(int[] A, int mid, int n) {
+      int l = 0, h = n - 1;
+      while (l <= h) {
+        int md = (l + h) >> 1;
+        if (A[md] <= mid) {
+          l = md + 1;
+        } else {
+          h = md - 1;
+        }
+      }
+      return l;
+    }
+  
+    public static int Median(int[][] A, int row, int col) {
+      int low = 1;
+      int high = 0;
+      for (int j = 0; j < row; j++) {
+        high = Math.max(high, A[j][col - 1]);
+      }
+      int n = row;
+      int m = col;
+      while (low <= high) {
+        int mid = (low + high) >> 1;
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+          cnt += countSmallerThanMid(A[i], mid, m);
+        }
+        if (cnt <= (n * m) / 2)
+          low = mid + 1;
+        else
+          high = mid - 1;
+      }
+      return low;
+    }
+  
+    public static int findMedian(int matrix[][], int m, int n) {
+      return Median(matrix, m, n);
+    }
+  }
+  
