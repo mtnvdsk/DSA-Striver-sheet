7c2e7e0f322860922db4af4284e8425ba946ab6c
mtnvdsk <mtnvdsk@gmail.com>
2024-03-12 19:06:19 +0530

    Heap

---
+++ Heaps/Hard problems/Median in a stream.java
@@ -0,0 +1,27 @@
+import java.util.PriorityQueue;
+import java.util.Collections;
+
+public class Solution {
+    public static int[] findMedian(int[] arr, int n) {
+        int[] medians = new int[n];
+        PriorityQueue<Integer> lo = new PriorityQueue<>();
+        PriorityQueue<Integer> hi = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            int num = arr[i];
+            lo.add(num);
+            hi.add(lo.poll());
+            if (lo.size() < hi.size()) {
+                lo.add(hi.poll());
+            }
+            int median;
+            if (lo.size() > hi.size()) {
+                median = lo.peek();
+            }
+            else {
+                median = (lo.peek() + hi.peek()) / 2;
+            }
+            medians[i] = median;
+        }
+        return medians;
+    }
+}
